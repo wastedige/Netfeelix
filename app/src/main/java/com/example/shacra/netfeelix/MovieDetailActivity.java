ff0e7f1e8f3c54252f6410076abd7d5e082fac90
Shaahin <shaahin@gmail.com>
2016-01-05 01:08:05 -0600

    Basic detail page populates

--- app/src/main/java/com/example/shacra/netfeelix/MovieDetailActivity.java
+++ app/src/main/java/com/example/shacra/netfeelix/MovieDetailActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,11 +17,18 @@ public class MovieDetailActivity extends Activity {
 
     String jsonString;
     int position;
+    ImageView detailed_image;
+    TextView detailed_text_title;
+    TextView detailed_text_overview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        detailed_image = (ImageView) findViewById(R.id.detailed_image);
+        detailed_text_title = (TextView) findViewById(R.id.detailed_text_title);
+        detailed_text_overview = (TextView) findViewById(R.id.detailed_text_overview);
 
         Intent intent = getIntent();
         jsonString = intent.getStringExtra("movieJson");
@@ -31,21 +42,30 @@ public class MovieDetailActivity extends Activity {
         }
     }
 
-    private String[] parseMovieDetailsFromJson(String movieJsonStr, int position)
+    private void parseMovieDetailsFromJson(String movieJsonStr, int position)
             throws JSONException {
         JSONObject movieJson = new JSONObject(movieJsonStr);
         JSONArray resultsArray = movieJson.getJSONArray("results");
         // single out the JSON string for the selected movie
         JSONObject singleMovie = resultsArray.getJSONObject(position);
 
-        String imageAddress = singleMovie.getString("poster_path");
-        String overview = singleMovie.getString("overview");
 
-        Log.v("LOG", overview + "");
+//        Log.v("LOG", overview + "");
+        showMovieDetails(
+                singleMovie.getString("poster_path"),
+                singleMovie.getString("overview")
+        );
 
+        return;
+    }
 
+    private void showMovieDetails(String... params) {
 
+        Glide.with(this)
+                .load("http://image.tmdb.org/t/p/w780/" + params[0])
+                .into(detailed_image);
 
-        return null;
+        detailed_text_overview.setText(params[1]);
+
     }
 }
